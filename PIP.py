27af6d5a2594d0fb12792ffb1bc7485b50265624
mulwasteveh <stephenmulwa344@gmail.com>
2021-03-08 13:16:26 +0300

    converting all start-letters to upercase

---
+++ PIP.py
@@ -0,0 +1,5 @@
+import camelcase
+#This method capitalizes the first letter of each word.
+sample = camelcase.CamelCase()
+text = "kibabii university is situated in bungoma"
+print(sample.hump(text))
